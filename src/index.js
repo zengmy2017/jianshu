21c10bb972f9d0627e76ece4d5927c28574b65f7
zengmy2017 <zengmy@gmail.com>
2020-10-11 20:59:13 +0800

    add to jianshu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,Link,Route,Switch} from 'react-router-dom';
+
 import App from './App';
-import Home from "./page/Home";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
